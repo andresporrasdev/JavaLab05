03f5c0b3eaf1eb3fe4d4d15e96c60d1a59cfc3a6
andresporrasdev <camilop1730@gmail.com>
2023-11-07 01:29:38 -0500

    Added constructor and BiWeeklyPay() to calculate the salary of the supervisor

--- Lab05/src/polymorphism/Supervisors.java
+++ Lab05/src/polymorphism/Supervisors.java
@@ -9,18 +9,24 @@ You must implement the bi-weekly pay for the employee according to instructions 
 
 // TO DO: TASK 1 - YOUR CODE FOR THE CLASS GOES HERE
 // Declare the class and any variable
-
-/**
+public class Supervisors extends Employees{
+	double bonusSupervisor;
+	// YOUR Constructor code goes here!!!
+	/**
     TO DO: TASK 2 - Constructs a supervisor with a given name, annual pay and bi-weekly bonus.
    @param name the name of this employee
    @param pay the annual pay
    @param bonus the bi-weekly bonus
  */
-// YOUR Constructor code goes here!!!
-
-
-// TO DO: TASK 3 - YOUR CODE FOR COMPUTING THE BI-WEEKLY PAY FOR THIS CATEGORY OF MILLER GOES HERE.
-
+	Supervisors(String name, double wage, double bonus){
+		super(name, wage);
+		bonusSupervisor = bonus;
+	}
+	// TO DO: TASK 3 - YOUR CODE FOR COMPUTING THE BI-WEEKLY PAY FOR THIS CATEGORY OF MILLER GOES HERE.
+	@Override
+	public double biWeeklyPay(int hoursWorked){
+		return hoursWorked*bonusSupervisor;
+	}
 // Hint: Note the relationship between this class and any other class. How will you apply that to your code?
 
 }
