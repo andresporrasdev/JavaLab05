6f609b93b2dc1102c9212a530661a81ca524b9b1
andresporrasdev <camilop1730@gmail.com>
2023-11-06 23:49:39 -0500

    Initial files provided

---
+++ Lab05/src/polymorphism/Employees.java
@@ -0,0 +1,26 @@
+
+package polymorphism;
+
+/**
+This class represents the employee. An employee is paid the same (particular) amount regardless of the hours (additional) the employee worked.
+*/
+
+TO DO: TASK 1 - DECLARE YOUR CLASS AND ANY VARIABLES HERE.
+/**
+
+   Constructs an employee with a given name and an annual pay.
+   @param name the name of this employee
+   @param pay the annual pay of the employee
+ */
+public Staff(String name, double pay)
+{
+   setName(name);
+   annualPay = pay;
+}
+
+// TO DO: TASK - This portion of your code computes the pay for this category of miller.
+
+// YOUR CODE GOES IN HERE.
+
+}
+
