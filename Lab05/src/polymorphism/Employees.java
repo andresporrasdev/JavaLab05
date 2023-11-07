bab727279b514c345652435adebc3f67a69c113a
andresporrasdev <camilop1730@gmail.com>
2023-11-07 01:30:46 -0500

    Declared class and variables of class Added the biWeeklyPay() for computer the payment of the miller

--- Lab05/src/polymorphism/Employees.java
+++ Lab05/src/polymorphism/Employees.java
@@ -1,26 +1,30 @@
 
 package polymorphism;
 
-/**
-This class represents the employee. An employee is paid the same (particular) amount regardless of the hours (additional) the employee worked.
-*/
+public class Employees extends Millers {
 
-TO DO: TASK 1 - DECLARE YOUR CLASS AND ANY VARIABLES HERE.
-/**
-
-   Constructs an employee with a given name and an annual pay.
-   @param name the name of this employee
-   @param pay the annual pay of the employee
- */
-public Staff(String name, double pay)
-{
-   setName(name);
-   annualPay = pay;
-}
-
-// TO DO: TASK - This portion of your code computes the pay for this category of miller.
-
-// YOUR CODE GOES IN HERE.
-
+	/**
+	This class represents the employee. An employee is paid the same (particular) amount regardless of the hours (additional) the employee worked.
+	*/
+	
+	// TO DO: TASK 1 - DECLARE YOUR CLASS AND ANY VARIABLES HERE.
+	private double annualPay;
+	/**
+	
+	   Constructs an employee with a given name and an annual pay.
+	   @param name the name of this employee
+	   @param pay the annual pay of the employee
+	 */
+	public Employees(String name, double pay) {
+	   setName(name);
+	   annualPay = pay;
+	}
+	
+	// TO DO: TASK - This portion of your code computes the pay for this category of miller.
+		@Override
+		public double biWeeklyPay(int hoursWorked) {
+			// YOUR CODE GOES IN HERE.
+			return annualPay/52;
+		}
 }
 
