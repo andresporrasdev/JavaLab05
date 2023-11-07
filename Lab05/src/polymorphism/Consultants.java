1c58001f369c9aba2d61aec365c556f9480209c2
andresporrasdev <camilop1730@gmail.com>
2023-11-07 01:32:44 -0500

    Create the class consultants and class variables Added a constructor for the class Added the function biWeeklyPay for calculate the pay of the consultant

--- Lab05/src/polymorphism/Consultants.java
+++ Lab05/src/polymorphism/Consultants.java
@@ -5,32 +5,34 @@ package polymorphism;
 This code in this file is for the Consultant Class. A Consultant is a miller who is paid for every hour worked.
 */
 // TO DO: TASK 1 - YOUR CODE GOES HERE TO DECLARE THE CONSULTANT CLASS AND ITS VARIABLES.
+class Consultants extends Millers{
+	/**
+	 * 
+	   TO DO: TASK 2 - This portion of your code constructs an hourly paid consultant with a given name and biweekly wage.
+	   @param name the name of this consultant
+	   @param wage the consultant wage per hour 
+	*/
+	private double consultantWage;
+	public Consultants(String name, double wage) {
+		super();
+		setName(name);
+		consultantWage=wage;
+	}
 
-
-/**
-   TO DO: TASK 2 - This portion of your code constructs an hourly paid consultant with a given name and biweekly wage.
-   @param name the name of this consultant
-   @param wage the consultant wage per hour 
-*/
-// YOUR CODE FOR YOUR CONSULTANT GOES HERE!!
-
-
-
-// TO DO: TASK 3 - THIS PORTION OF YOUR CODE COMPUTES THE 
-
-public double biWeeklyPay(int hoursWorked)
-{
-   double pay = hoursWorked * consultantWage;
-
-// In this part of your code, an overtime amount is added if the Consultant works for more than 76 hours bi-weekly.
-// Review this portion of code carefully to include the code to fully satisfy this condition.
-  
-// SOME CODE GOES HERE BEFORE THE CODE BELOW:
-    
-      pay = pay + ((hoursWorked - 76) * 0.5) * consultantWage;
-   
-   return pay;
-}
+	// TO DO: TASK 3 - THIS PORTION OF YOUR CODE COMPUTES THE 
+	@Override
+	public double biWeeklyPay(int hoursWorked) {
+	   double pay = hoursWorked * consultantWage;
+	
+	// In this part of your code, an overtime amount is added if the Consultant works for more than 76 hours bi-weekly.
+	// Review this portion of code carefully to include the code to fully satisfy this condition.
+	  
+	// SOME CODE GOES HERE BEFORE THE CODE BELOW:
+	    
+	      pay = pay + ((hoursWorked - 76) * (0.5 * consultantWage));
+	   
+	   return pay;
+	}
 }
 
 
