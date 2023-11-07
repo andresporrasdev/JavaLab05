4561c02c11c56c0adee826b8c203b6e13c15733f
andresporrasdev <camilop1730@gmail.com>
2023-11-07 01:28:21 -0500

    Added JUnit 4 Test

---
+++ Lab05/src/polymorphism/TestLab05.java
@@ -0,0 +1,13 @@
+package polymorphism;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class TestLab05 {
+
+	@Test
+	public void test() {
+		Consultants testConsultant = new Consultants("Prueba1", 50);
+		System.out.println(testConsultant.getName());
+	}
+}
